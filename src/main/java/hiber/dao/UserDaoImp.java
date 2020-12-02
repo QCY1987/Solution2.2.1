3c74e0af9da36790c39a51480e3ad51ba5c8ca51
Антон <avdev1987@gmail.com>
2020-12-03 01:09:03 +0500

    2.2.1 Finally solution

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,74 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      Session session = sessionFactory.openSession();
+      final Transaction transaction = session.getTransaction();
+      try {
+         sessionFactory.getCurrentSession().save(user);
+      } catch (Exception e) {
+         e.printStackTrace();
+         transaction.rollback();
+      } finally {
+         session.close();
+      }
+
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      Session session = sessionFactory.openSession();
+      final Transaction transaction = session.getTransaction();
+      Query query = null;
+      try {
+         query = sessionFactory.getCurrentSession().createQuery("from User");
+      } catch (Exception e) {
+         e.printStackTrace();
+         transaction.rollback();
+      } finally {
+         session.close();
+      }
+      return query.getResultList();
+   }
+
+   @Override
+   public List<User> getUsersCar(String model, int series) {
+      Session session = sessionFactory.openSession();
+      final Transaction transaction = session.getTransaction();
+      Query query = null;
+      transaction.begin();
+      try {
+         query = sessionFactory.getCurrentSession().createQuery(
+                 "SELECT u FROM User u " +
+                         "JOIN FETCH u.car " +
+                         "WHERE model = :paramNumber and series = :paramSeries");
+         query.setParameter("paramNumber", model);
+         query.setParameter("paramSeries", series);
+         transaction.commit();
+      } catch (Exception e) {
+         e.printStackTrace();
+         transaction.rollback();
+      } finally {
+         session.close();
+      }
+      return query.getResultList();
+   }
+}
